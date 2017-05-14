ee9c48a4f89da8bf32f4a902004ce692a35f5374
Rohit Motwani <rohittmotwani@gmail.com>
2017-05-15 02:10:03 +0530

    work till now

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var name = prompt("What is your Name ?");
-alert("Hello, " + name);
+var ar = new Array();
+
+while(input!="quit"){
+    var input = prompt("What do you want to do ?");
+    if(input==="new"){
+        var item = prompt("Enter The Item");
+        ar.push(item);
+    }
+    else if(input=="list") {
+        console.log(ar);
+    }
+}
+console.log("QUIT");
